55910081b47d1f1a20f20e400083d5b4e4cfc965
Alex <alexander.schmidt90@gmail.com>
2018-04-02 16:50:12 +0200

    Add Wavespawner and UI

--- Simple-TD/Assets/Waypoints.cs
+++ Simple-TD/Assets/Waypoints.cs
@@ -4,7 +4,7 @@ public class Waypoints : MonoBehaviour {
 
     public static Transform[] points;
 
-    void Awayke ()
+    void Awake ()
     {
         points = new Transform[transform.childCount];
         for (int i = 0; i < points.Length; i++)
