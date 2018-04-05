4c4651600edab86af4d8b8f529fba99599101179
Alex <alexander.schmidt90@gmail.com>
2018-04-05 21:01:06 +0200

    Add Camera Pan and Zoom

---
+++ Simple-TD/Assets/BuildManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+        instance = this;    
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private GameObject turretToBuild;
+
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab;    
+    }
+
+    
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+
+}
