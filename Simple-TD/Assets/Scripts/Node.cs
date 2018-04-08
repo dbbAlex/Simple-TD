e45e87a029f055643552bcbe01d7b9806c31c70c
Alex <alexander.schmidt90@gmail.com>
2018-04-08 23:36:42 +0200

    Add CurrencySystem and some Refactoring

---
+++ Simple-TD/Assets/Scripts/Node.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    private Color startColor;
+    public Vector3 positionOffset;
+
+    private Renderer rend;
+
+    [Header("Optional")]
+    public GameObject turret;
+
+    BuildManager buildManager;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+
+        buildManager = BuildManager.instance;
+    }
+
+    public Vector3 GetBuildPosition ()
+    {
+        return transform.position + positionOffset;
+    }
+
+    void OnMouseDown()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        if (!buildManager.CanBuild)
+        {
+            return;
+        }
+
+        if (turret != null)
+        {
+            Debug.Log("Can't build there! - TODO: Display on screen.");
+            return;
+        }
+
+        buildManager.BuildTurretOn(this);
+    }
+
+    void OnMouseEnter()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        if (!buildManager.CanBuild)
+        {
+            return;
+        }
+
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;    
+    }
+
+}
---
+++ Simple-TD/Assets/Scripts/Shop.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+    public TurretBlueprint standardTurret;
+    public TurretBlueprint missileLauncher;
+
+    BuildManager buildManager;
+
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+    public void SelectStandardTurret ()
+    {
+        Debug.Log("Standard");
+        buildManager.SelectTurretToBuild(standardTurret);
+    }
+
+    public void SelectMissileLauncher ()
+    {
+        Debug.Log("Missile");
+        buildManager.SelectTurretToBuild(missileLauncher);
+    }
+
+}
